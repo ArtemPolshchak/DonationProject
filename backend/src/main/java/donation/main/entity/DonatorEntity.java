dc2108b988a0818998c404f890f06fb60ed378b4
Артем Польщак <artem.polschak@gmail.com>
2024-02-07 00:52:53 +0200

    add login component on front

--- backend/src/main/java/donation/main/entity/DonatorEntity.java
+++ backend/src/main/java/donation/main/entity/DonatorEntity.java
@@ -7,8 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 
@@ -16,13 +19,15 @@ import java.math.BigDecimal;
 @Table(name = "donators")
 @Getter
 @Setter
+@ToString(of = {"email", "totalDonations"})
+@NoArgsConstructor
 public class DonatorEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "email", unique = true)
+    @Column(name = "email", unique = true, length = 32)
     private String email;
 
     @Column(name = "total_donations")
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,15 @@ import {TransactionComponent} from "./components/transaction/transaction.compone
 import {DonatorsComponent} from "./components/donators/donators.component";
 import {ServerComponent} from "./components/server/server.component";
 import {UserComponent} from "./components/user/user.component";
+import {LoginComponent} from "./components/login/login.component";
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: "full"},
+    {path: 'login', component: LoginComponent},
     {path: 'dashboard', component: DashboardComponent},
     {path: 'donations', component: TransactionComponent},
     {path: 'donators', component: DonatorsComponent},
     {path: 'servers', component: ServerComponent},
     {path: 'users', component: UserComponent}
+
 ];
