9a6d6414dab8b203902547d1499c01a06e142135
Артем Польщак <artem.polschak@gmail.com>
2024-02-18 23:31:28 +0200

    fixed bag with loading servers in sessionStorage

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,7 +3,7 @@ import {SidebarComponent} from "./components/sidebar/sidebar.component";
 import {Router, RouterOutlet} from "@angular/router";
 import {NgIf} from "@angular/common";
 import {AuthService} from "./services/auth.service";
-import {ServerService} from "./services/server.service";
+
 
 @Component({
     selector: 'app-root',
@@ -19,21 +19,11 @@ import {ServerService} from "./services/server.service";
 export class AppComponent implements OnInit{
 
     constructor(public authService: AuthService,
-                private router: Router,
-                private serverService: ServerService) {}
+                private router: Router) {}
 
     ngOnInit(): void {
         if (!this.authService.isLoggedIn()) {
             this.router.navigateByUrl("/login")
         }
-        this.getServerList();
-    }
-
-    private getServerList(): void {
-        this.serverService.getAll().subscribe({
-            next: (v) =>
-                sessionStorage.setItem('servers', JSON.stringify(v.content)),
-            error: (e) => console.error(e),
-        });
     }
 }
