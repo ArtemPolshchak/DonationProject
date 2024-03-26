d537a1eb34f2a2f7b4f7997ae2f79609f948fca4
Артем Польщак <artem.polschak@gmail.com>
2024-03-26 19:38:55 +0200

    test add window on localstorage

--- frontend/src/app/services/storage.service.ts
+++ frontend/src/app/services/storage.service.ts
@@ -8,7 +8,7 @@ import {jwtDecode} from "jwt-decode";
 })
 export abstract class StorageService {
 
-    static storage: Storage = localStorage;
+    static storage: Storage = window.localStorage;
 
     static signOut(): void {
         this.storage.clear();
