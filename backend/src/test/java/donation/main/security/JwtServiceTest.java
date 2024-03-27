8067b6fac6586de80681586f5a26539bec9a85d1
Артем Польщак <artem.polschak@gmail.com>
2024-03-27 03:17:40 +0200

    add directory for testing

---
+++ backend/src/test/java/donation/main/security/JwtServiceTest.java
@@ -0,0 +1,30 @@
+package donation.main.security;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void extractUserEmail() {
+    }
+
+    @Test
+    void generateToken() {
+    }
+
+    @Test
+    void isTokenValid() {
+    }
+}
---
+++ backend/src/test/java/donation/main/service/ServerServiceTest.java
@@ -0,0 +1,54 @@
+package donation.main.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getAllServersNames() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void createDonatorsBonusOnServer() {
+    }
+
+    @Test
+    void updateDonatorsBonusOnServer() {
+    }
+
+    @Test
+    void getAllDonatorBonusesByServerId() {
+    }
+
+    @Test
+    void searchDonatorsByEmailLike() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
