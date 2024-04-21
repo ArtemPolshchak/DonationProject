d7c344e0c1eeb0805e8f60a4c4fd37279c30c4a8
Max Skory <devisrush@gmail.com>
2024-04-21 18:33:04 +0300

    checkstyle issues were resolved

--- backend/src/main/java/donation/main/repository/DonatorRepository.java
+++ backend/src/main/java/donation/main/repository/DonatorRepository.java
@@ -17,21 +17,21 @@ public interface DonatorRepository extends JpaRepository<DonatorEntity, Long> {
     Page<DonatorEntity> findByEmailContainingIgnoreCase(String email, Pageable pageable);
 
     @Query(nativeQuery = true, value =
-            "SELECT id, email, totalCompletedTransactions, COALESCE(sum, 0) as totalDonations " +
-                    "FROM (SELECT d.id AS id, d.email AS email,  " +
-                    "COUNT(t.contribution_amount) AS totalCompletedTransactions , " +
-                    "SUM(t.contribution_amount) AS sum " +
-                    "FROM donators AS d " +
-                    "LEFT JOIN transactions AS t ON t.donator_id = d.id " +
-                    "WHERE d.email LIKE LOWER(CONCAT('%', :email, '%')) " +
-                    "AND (:serverId IS NULL OR t.server_id = :serverId) " +
-                    "AND (t IS NULL OR t.state = 'COMPLETED') " +
-                    "GROUP BY d.id) AS sub",
-            countQuery = "SELECT COUNT(1) FROM donators as d " +
-                    "LEFT JOIN transactions AS t ON t.donator_id = d.id "  +
-                    "WHERE d.email LIKE LOWER(CONCAT('%', :email, '%')) " +
-                    "AND (:serverId IS NULL OR t.server_id = :serverId) " +
-                    "AND (t IS NULL OR t.state = 'COMPLETED') " +
-                    "GROUP BY d.id" )
+            "SELECT id, email, totalCompletedTransactions, COALESCE(sum, 0) as totalDonations "
+                    + "FROM (SELECT d.id AS id, d.email AS email,  "
+                    + "COUNT(t.contribution_amount) AS totalCompletedTransactions , "
+                    + "SUM(t.contribution_amount) AS sum "
+                    + "FROM donators AS d "
+                    + "LEFT JOIN transactions AS t ON t.donator_id = d.id "
+                    + "WHERE d.email LIKE LOWER(CONCAT('%', :email, '%')) "
+                    + "AND (:serverId IS NULL OR t.server_id = :serverId) "
+                    + "AND (t IS NULL OR t.state = 'COMPLETED') "
+                    + "GROUP BY d.id) AS sub",
+            countQuery = "SELECT COUNT(1) FROM donators as d "
+                    + "LEFT JOIN transactions AS t ON t.donator_id = d.id "
+                    + "WHERE d.email LIKE LOWER(CONCAT('%', :email, '%')) "
+                    + "AND (:serverId IS NULL OR t.server_id = :serverId) "
+                    + "AND (t IS NULL OR t.state = 'COMPLETED') "
+                    + "GROUP BY d.id")
     Page<DonatorTotalDonationsView> findTotalDonationsByEmailLikeAndServerName(String email, Long serverId, Pageable pageable);
 }
