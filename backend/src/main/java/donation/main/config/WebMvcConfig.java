9351fbdd3188762da81272f24174e4fbc8fbe83e
Artem <artem.polschak@gmail.com>
2024-06-02 23:07:24 +0200

    Test commit

--- backend/src/main/java/donation/main/config/WebMvcConfig.java
+++ backend/src/main/java/donation/main/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package donation.main.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
